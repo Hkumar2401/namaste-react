8839768f813a265c5189be3881bdf9e9bbba5145
Hemant Kumar <hemantk240103@gmail.com>
2024-05-10 09:36:27 +0530

    Episode-08

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,28 @@
-const About = () =>{
+import User from "./User";
+import UserClass from "./UserClass";
+import React from "react";
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent Component Did Mount");
+  }
+
+  render() {
+    console.log("Parent Render");
+
     return (
-        <div>
-            <h1>About Us</h1>
-        </div>
+      <div>
+        <h1>About Us</h1>
+        <UserClass name={"Hemant (class)"} location={"Delhi"} />
+      </div>
     );
+  }
 }
 
 export default About;
